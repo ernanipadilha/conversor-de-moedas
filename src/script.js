8e994f09f939be5f9b8d114708895b0de0c6215b
ernanipadilha <ernanipadilha@gmail.com>
2021-10-06 15:39:39 -0300

    Ajuste para pegar os valores das moedas de uma API

--- src/script.js
+++ src/script.js
@@ -1,22 +1,24 @@
-function Converter(dolar) {
-  atualizarValor()
+var requestURL = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,BRL-USD,BTC-USD,USD-EUR";
+var request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json"; // now we're getting a string!
+request.send();
+
+function Converter() {
+  var dolar = request.response;
   var valorDolar = parseFloat(document.getElementById("valor").value);
-  var valorDoDolar = parseFloat(exibeMoeda(dolar.USDBRL.high));
-  var valorConvertido = valorDolar * valorDoDolar;
+  var valorConvertido = parseFloat(valorDolar * dolar.USDBRL.high);
 
   var elementoValorConvertido = document.getElementById("valorConvertido");
   var exibeValor = "O valor em Real é R$ " + valorConvertido.toFixed(2);
   elementoValorConvertido.innerHTML = exibeValor;
-  
-
 
 }
 
 function ConverterEuro() {
-  var valorElemento = document.getElementById("valor");
-  var valor = valorElemento.value;
-  var valorEmDolarNumerico = parseFloat(valor);
-  var valorConvertido = valorEmDolarNumerico * 0.85;
+  var euro = request.response;
+  var valorDolar = document.getElementById("valor").value;
+  var valorConvertido = parseFloat(valorDolar * euro.USDEUR.high);
 
   var elementoValorConvertido = document.getElementById("valorConvertidoEuro");
   var exibeValor = "O valor em Euro é € " + valorConvertido.toFixed(2);
@@ -25,35 +27,14 @@ function ConverterEuro() {
 }
 
 function ConverterBit() {
-  var valorElemento = document.getElementById("valor");
-  var valor = valorElemento.value;
-  var valorEmDolarNumerico = parseFloat(valor);
-  var valorConvertido = valorEmDolarNumerico * 0.000021;
+  var btc = request.response;
+  var valorDolar = document.getElementById("valor").value;
+  var valorConvertido = valorDolar / btc.BTCUSD.high;
 
   var elementoValorConvertido = document.getElementById("valorConvertidoBit");
-  var exibeValor = "O valor em BitCoin é ₿ " + valorConvertido.toFixed(5);
+  var exibeValor = "O valor em BitCoin é ₿ " + valorConvertido.toFixed(10);
   elementoValorConvertido.innerHTML = exibeValor;
 
 }
 
-function exibeMoeda(moedinhas) {
-  var moedasPesquisadas = moedinhas;
-  console.log(moedasPesquisadas)
-  return moedasPesquisadas
-}
-
-var requestURL = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,BRL-USD,BTC-USD,USD-EUR";
-
-function atualizarValor(){
-var request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json"; // now we're getting a string!
-request.send();
-
- request.onload = function () {
-  var moedas = request.response; // get the string from the response
-  exibeMoeda(moedas)
-  //Converter(moedas)
-};
-}
 
